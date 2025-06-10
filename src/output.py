b29a461e5b9d3df1fc93245f5eabde40bd6aca7c
Cagrihann <cagrihancakmak@gmail.com>
2025-06-10 13:54:37 +0300

    Update output.py

--- src/output.py
+++ src/output.py
@@ -3,19 +3,15 @@ import numpy as np
 import joblib
 import functions as fn
 import os 
-import torch
-import torch.nn as nn
 import tensorflow as tf
 from tensorflow.keras import models
-from model_pythorch_ANN import ANN
 from sklearn.metrics import classification_report,r2_score,mean_absolute_error,root_mean_squared_error
 
 train_X = pd.read_csv("data/processed/3_class/6000_train_X.csv")
 train_y = pd.read_csv("data/processed/3_class/6000_train_y.csv")
 test_X = pd.read_csv("data/processed/3_class/6000_test_X.csv")
 test_y = pd.read_csv("data/processed/3_class/6000_test_y.csv")
-train_X_tensor = torch.tensor(train_X.values,dtype=torch.float32)
-test_X_tensor = torch.tensor(test_X.values,dtype=torch.float32)
+
 
 ACC = {}
 
@@ -70,36 +66,6 @@ for idx,(name, path) in enumerate(models_dict.items()):
         "RMSE_train": root_mean_squared_error(train_y,train_pred)
     }
     print(f"tamamlandi\n#############")
-
-
-##########################################################
-# Pytorch ile oluşturulan modelin test edilmesi
-##########################################################    
-
-ANN_py = ANN()
-ANN_py.load_state_dict(torch.load(f="models/pytorch/ANN10x10x10.pth",weights_only=True))
-ANN_py.eval()
-with torch.inference_mode():
-    torch_test_pred_proba = ANN_py(test_X_tensor) #çünkü softmax kullanıldı
-    torch_test_pred_class = torch.argmax(torch_test_pred_proba, dim=1)
-    torch_train_pred_proba = ANN_py(train_X_tensor)
-    torch_train_pred_class = torch.argmax(torch_train_pred_proba, dim=1)
-auc = fn.roc_curve_plot(test_y,torch_test_pred_proba,"PyTorch 10x10x10",class_names)
-fn.conf_mat(test_y,torch_test_pred_class,class_names=class_names,Model_Name="PyTorch 10x10x10")
-report = classification_report(test_y,torch_test_pred_class,output_dict=True)
-ACC["PyTorch 10x10x10"] = {
-        "Accuracy": report["accuracy"],
-        "Precision": report["macro avg"]["precision"],
-        "Sensivity": report["macro avg"]["recall"],
-        "F-measure": report["macro avg"]["f1-score"],
-        "ROC area": pd.Series(auc).mean(),
-        "R2_test": r2_score(test_y,torch_test_pred_class),
-        "MAE_test": mean_absolute_error(test_y,torch_test_pred_class),
-        "RMSE_test": root_mean_squared_error(test_y,torch_test_pred_class),
-        "R2_train": r2_score(train_y,torch_train_pred_class),
-        "MAE_train": mean_absolute_error(train_y,torch_train_pred_class),
-        "RMSE_train": root_mean_squared_error(train_y,torch_train_pred_class)
-    }
 
 ##########################################################
 # Tensorflow ile oluşturulan modelin test edilmesi
